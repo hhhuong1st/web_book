37c2b47d890b85c756bc317f1d308fb3f9492f6c
h <huynhhuyenhuong2006@gmail.com>
2026-02-11 13:23:33 +0700

    a

--- main-files/assets/js/components.js
+++ main-files/assets/js/components.js
@@ -62,22 +62,29 @@ const Header = () => {
 const Footer = () => {
     return (
         <footer className="site-footer">
-            <div className="footer-wave"></div>
+            {/* SVG Wave Separator */}
+            <div className="footer-wave-svg">
+                <svg viewBox="0 0 1440 320" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none">
+                    {/* Yellow Wave (Background) - More prominent on left */}
+                    <path fill="#FACC15" fillOpacity="1" d="M0,60 C150,60 250,240 400,240 C560,240 720,220 880,190 C1040,160 1240,80 1400,70 L1440,60 L1440,320 L0,320 Z"></path>
+                    {/* Black Wave (Foreground) */}
+                    <path fill="#111" fillOpacity="1" d="M0,128L80,144C160,160,320,192,480,197.3C640,203,800,181,960,197.3C1120,213,1280,267,1360,293.3L1440,320L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path>
+                </svg>
+            </div>
 
-            {/* Character Image - Moved outside container for better positioning */}
+            {/* Character Image */}
             <div className="footer-character">
                 <img src="images/imgi_65_footer-left-img.png" alt="Demon Character" />
             </div>
 
-            <div className="container relative text-center" style={{ position: 'relative', zIndex: '10' }}>
-
-                {/* Top Section: Logo, Button, Socials */}
+            <div className="container relative z-10" style={{ position: 'relative', zIndex: 20 }}>
+                {/* Top Section */}
                 <div className="footer-top">
                     <div className="footer-logo">Comixs.</div>
-                    <button className="btn-buy-now">Buy Now</button>
+                    <button className="btn-buy-now-footer">Buy Now</button>
                     <div className="footer-socials">
                         <i className="fa-brands fa-facebook-f"></i>
-                        <i className="fa-brands fa-twitter"></i>
+                        <i className="fa-brands fa-x-twitter"></i> {/* Updated to X icon if available, else twitter */}
                         <i className="fa-brands fa-linkedin-in"></i>
                         <i className="fa-brands fa-instagram"></i>
                     </div>
@@ -85,37 +92,46 @@ const Footer = () => {
 
                 <hr className="footer-divider" />
 
-                {/* Middle Section: Contact Info */}
+                {/* Info Section */}
                 <div className="footer-info">
                     <div className="info-item">
-                        <i className="fa-regular fa-envelope text-yellow"></i>
-                        <div className="text-left">
-                            <span className="info-label text-yellow">Email Id</span>
+                        <div className="info-icon">
+                            <i className="fa-regular fa-envelope"></i>
+                        </div>
+                        <div className="info-text">
+                            <span className="info-label">Email Id</span>
                             <span className="info-val">exmaple@gmail.com</span>
                         </div>
                     </div>
                     <div className="info-item">
-                        <i className="fa-solid fa-phone text-yellow"></i>
-                        <div className="text-left">
-                            <span className="info-label text-yellow">Phone</span>
+                        <div className="info-icon">
+                            <i className="fa-solid fa-phone"></i>
+                        </div>
+                        <div className="info-text">
+                            <span className="info-label">Phone</span>
                             <span className="info-val">(+33)7 35 55 31 15</span>
                         </div>
                     </div>
                     <div className="info-item">
-                        <i className="fa-solid fa-location-dot text-yellow"></i>
-                        <div className="text-left">
-                            <span className="info-label text-yellow">Address</span>
+                        <div className="info-icon">
+                            <i className="fa-solid fa-location-dot"></i>
+                        </div>
+                        <div className="info-text">
+                            <span className="info-label">Address</span>
                             <span className="info-val">(6391 Elgin St. Celina, Delaware 10299</span>
                         </div>
                     </div>
                 </div>
             </div>
 
-            {/* Bottom Section: Copyright - Moved outside container */}
+            {/* Copyright Section */}
             <div className="footer-copyright">
                 <div className="container">
                     Copyright © 2024 <span className="text-yellow">Comic Book Store Theme</span>. All Rights Reserved.
                 </div>
+                <button className="btn-back-to-top" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
+                    <i className="fa-solid fa-chevron-up"></i>
+                </button>
             </div>
         </footer>
     );
